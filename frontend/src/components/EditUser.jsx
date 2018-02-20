16f4baef75b941ac628198f1d9edd14183f57b58
helencho <helencho@ac.c4q.nyc>
2018-02-19 21:00:57 -0500

    move editing user form class names to temp

--- frontend/src/components/EditUser.jsx
+++ frontend/src/components/EditUser.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
 import '../stylesheets/app.css';
@@ -7,103 +7,144 @@ class EditUser extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: '',
-            fullname: '',
-            profilepicUrl: '',
-            userdescription: '',
-            email: '',
+            newName: '',
+            newEmail: '',
+            newFullName: '',
+            newProfilePic: '',
+            newDescription: '',
             editing: false
         }
     }
 
-    switchMode = () => {
-        const lastMode = this.state.editing;
-        return this.setState({
-            editing: !lastMode
-        })
-        console.log("Currently changing info/ editing?:", this.state.editing)
-    };
+    // switchMode = () => {
+    //     const lastMode = this.state.editing 
+    //     this.setState({
+    //         editing: !lastMode
+    //     })
+    // };
 
-    handleInputChange = e => {
+    handleInput = e => {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
-    submitForm = e => {
+    handleCancel = () => {
+        console.log('hitting cancel')
+        // return <Redirect to='/profile' />
+    }
+
+    handleSubmit = e => {
         e.preventDefault()
-        const {username, fullname, profilepicUrl, userdescription, email} = this.state
+        const { newName, newEmail, newFullName, newProfilePic, newDescription } = this.state
 
         this.setState({
             editing: !this.state.editing
         })
 
-        console.log("the state when the submitForm:", this.state)
-        console.log("id", this.props.user.user_id)
-
-        fetch(`/users/u/${this.props.user.user_id}/edit`, {
-            headers: {
-                "ACCEPT": "application/json",
-                "Content-Type": "application/json"
-            },
-            method: "PATCH",
-            body: JSON.stringify({
-                newName: username,
-                newEmail: email,
-                newFullname: fullname,
-                newProfile_pic: profilepicUrl,
-                newDescription: userdescription,
-                id: this.props.user.user_id
+        axios
+            .patch(`/users/u/:id/edit`, {
+                newName: newName,
+                newEmail: newEmail,
+                newFullName: newFullName,
+                newProfilePic: newProfilePic,
+                newDescription: newDescription
             })
-        })
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    editing: !this.state.editing 
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
     };
 
     render() {
-        console.log("state:", this.state)
-        console.log("props:", this.props)
-        const {
-            username,
-            fullname,
-            profilepicUrl,
-            userdescription,
-            email,
-            editing
-        } = this.state;
-            return (
-                <div id="form-main">
-  <div id="form-div">
-    <form className="form" id="form1" onSubmit={this.submitForm}>
-      
-      <p className="name">
-        <input name="username" type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input editInput" placeholder="Full Name" id="username" />
-      </p>
-      
-      <p className="fullname">
-        <input name="fullname" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="Username" />
-      </p>
-      <p className="profilepicUrl">
-        <input name="profilepicUrl" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="Email" />
-      </p>
-      <p className="email">
-        <input name="email" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="New Profile Pic" />
-      </p>
-      
-      <p className="text">
-        <textarea name="userdescription" className="validate[required,length[6,300]] feedback-input editInput" id="comment" placeholder="User Description"></textarea>
-      </p>
-      
-      
-      <div className="submit-BTN">
-        <input type="submit" id="button-blue" value="Submit"/>
-        <div className="ease"></div>
-      </div>
-    </form>
-    {editing && (
-          <Redirect to={'profile/'}/>
-        )}
-  </div>
-  </div>
-            )
+        const { newName, newEmail, newFullName, newProfilePic, newDescription, editing } = this.state
+        console.log(this.state)
+
+        return (
+            <div className='edit-user-container'>
+                <form className='edit-user-form' onSubmit={this.handleSubmit}>
+                    <input
+                        name='newName'
+                        type='text'
+                        placeholder='Username'
+                        value={newName}
+                        onChange={this.handleInput} />
+                    <input
+                        name='newFullName'
+                        type='text'
+                        placeholder='Full name'
+                        value={newFullName}
+                        onChange={this.handleInput} />
+                    <input
+                        name='newEmail'
+                        type='text'
+                        placeholder='Email'
+                        value={newEmail}
+                        onChange={this.handleInput} />
+                    <input
+                        name='newProfilePic'
+                        type='text'
+                        placeholder='Profile pic url'
+                        value={newProfilePic}
+                        onChange={this.handleInput} />
+                    <textarea
+                        name='newDescription'
+                        placeholder='Description about you'
+                        value={newDescription}
+                        onChange={this.handleInput} />
+                    <input
+                        type='button'
+                        value='Cancel'
+                        onClick={this.handleCancel} />
+                    <input
+                        type='submit'
+                        value='Save' />
+                </form>
+
+                {/* {editing && (
+                    <Redirect to={'profile/'} />
+                )} */}
+
+            </div>
+            // <div id="form-main">
+            //     <div id="form-div">
+            //         <form className="form" id="form1" onSubmit={this.submitForm}>
+
+            //             <p className="name">
+            //                 <input name="username" type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input editInput" placeholder="Full Name" id="username" />
+            //             </p>
+
+            //             <p className="fullname">
+            //                 <input name="fullname" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="Username" />
+            //             </p>
+            //             <p className="profilepicUrl">
+            //                 <input name="profilepicUrl" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="Email" />
+            //             </p>
+            //             <p className="email">
+            //                 <input name="email" type="text" className="validate[required,custom[email]] feedback-input editInput" id="email" placeholder="New Profile Pic" />
+            //             </p>
+
+            //             <p className="text">
+            //                 <textarea name="userdescription" className="validate[required,length[6,300]] feedback-input editInput" id="comment" placeholder="User Description"></textarea>
+            //             </p>
+
+
+            //             <div className="submit-BTN">
+            //                 <input type="submit" id="button-blue" value="Submit" />
+            //                 <div className="ease"></div>
+            //             </div>
+            //         </form>
+            //         {editing && (
+            //             <Redirect to={'profile/'} />
+            //         )}
+            //     </div>
+            // </div>
+        )
     }
 };
 export default EditUser
